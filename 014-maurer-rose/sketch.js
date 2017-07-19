58e1b9b22ea9451537761f6f12fe0ac203174177
Kaio Turubia <kaio_kot@hotmail.com>
2017-07-19 17:30:22 -0300

    adding  new algorithm , random draws

--- 014-maurer-rose/sketch.js
+++ 014-maurer-rose/sketch.js
@@ -4,55 +4,130 @@
 var previousVector;
 var n;
 var d;
+var k = 120;
+var interval;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   background(0)
-
-  n = int(random(1, 359));
-  d = int(random(1, 359));
-  fill(255);
-  text("refresh to new Maurer Rose", height / 2, 20);
-  text("n:" + n, 10, 30);
-  text("d:" + d, 10, 40);
-
-  maurerRose(n, d);
+  interval = 100;
 }
 
+function draw() {
+  if (frameCount % interval == 0) {
+    background(0)
 
-function maurerRose(n, d) {
-  translate(width / 2, height / 2);
+    n = int(random(1, 359));
+    d = int(random(1, 359));
+
+    fill(255);
+    
+    text("Random Maurer Rose", height / 2, 20);
+    
+    text("n:" + n, 10, 30);
+    text("d:" + d, 10, 40);
+
+    text("algorithm A:", 150, 50);
+    text("algorithm B:", 400, 50);
+    
+    maurerRoseAlgA(n, d, 400, 350);
+    
+    maurerRoseAlgB(n, d, 900, 350);
+  }
+
+}
+
+function maurerRoseAlgA(n, d, xxx, yyy) {
 
   var theta = 0;
 
-  previousVector = createVector();
+  previousVector = createVector(xxx / 2, yyy / 2);
 
   do {
-    theta = theta + d;
+    theta += d;
 
-    if (theta >= 360)
+    if (theta >= 360) {
       theta = theta % 360;
+    }
 
     var aux = theta % 360;
-    var x = (n * aux);
-    var r = 100 * sin(x);
-
+    var x = n * aux;
+    var r = k * sin(radians(x));
+    var t = radians(theta);
 
     // translating polar to rectangular coordinates
-    x = r * cos(theta);
-    y = r * sin(theta);
+    x = r * cos(t) + xxx / 2;
+    y = r * sin(t) + yyy / 2;
 
     var newVector = createVector(x, y);
 
     strokeWeight(0.25);
     stroke(random(255), random(255), random(255));
     line(previousVector.x, previousVector.y, newVector.x, newVector.y);
-
     previousVector = createVector(newVector.x, newVector.y);
-
   } while (theta != 0);
 }
 
-function draw() {
 
+function maurerRoseAlgB(n, d, xxx, yyy) {
+
+  var theta = 0;
+  var T = 0;
+  var c = 0;
+
+  previousVector = createVector();
+
+  var fixLines = true;
+
+  while (true) {
+
+    if (fixLines) {
+      theta = T;
+      var a = (TWO_PI * theta) / 360;
+      var rr = n * sin(radians(a));
+
+      var xx = rr * cos(radians(theta)) + xxx / 2;
+      var yy = rr * sin(radians(theta)) + yyy / 2;
+
+      previousVector = createVector(xx, yy);
+    }
+
+    theta += d;
+
+    if (theta >= 360) {
+      theta = theta % 360;
+    }
+
+    var aux = theta % 360;
+    var x = n * aux;
+    var r = k * sin(radians(x));
+
+    var t = radians(theta);
+
+    // translating polar to rectangular coordinates
+    x = r * cos(t) + xxx / 2;
+    y = r * sin(t) + yyy / 2;
+
+    var newVector = createVector(x, y);
+
+    strokeWeight(0.25);
+    stroke(random(255), random(255), random(255));
+    line(previousVector.x, previousVector.y, newVector.x, newVector.y);
+    c++;
+
+    if (theta == T) {
+      if (c >= 360) {
+        break;
+      }
+      else {
+        T++;
+        fixLines = true;
+      }
+    }
+    else {
+      previousVector = createVector(newVector.x, newVector.y);
+      fixLines = false;
+    }
+  }
 }
+
